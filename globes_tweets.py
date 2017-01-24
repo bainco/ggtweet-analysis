108e6d684e2469f625211b3df8fdc7be0ec35bcf
Connor Bain <connorbain@u.northwestern.edu>
2017-01-24 10:19:58 -0600

    Add method to read tweet metadata and twitter tokenizer

--- globes_tweets.py
+++ globes_tweets.py
@@ -1,6 +1,30 @@
-import sys
+import sys, csv
+from nltk.tokenize import TweetTokenizer
 
-def read_tweets(fname):
+# This might be useful:
+# http://www.nltk.org/howto/twitter.html
+
+
+def read_tweets_with_metadata(fname):
+    """ Alternate method to read in the tweets with metadata text file. This
+    also returns a list of tweets, but instead of just strings, they're
+    dicts with some other stuff in them.
+
+    returns: list of tweets
+    """
+    tweets = []
+
+    with open(fname,'r') as f:
+        reader=csv.reader(f,delimiter='\t')
+        for text, tweeter, thingA, thingB, timestamp in reader:
+            theTweet = {"text": text, "tweeter": tweeter, "thingA":thingA, "thingB":thingB, "timestamp":timestamp}
+            tweets.append(theTweet)
+
+    # Return reverse since the file was read in newest to oldest
+    return tweets.reverse()
+
+def read_tweets(fname, simple=True):
+
     tweets = []
     tweets_file = open(fname, 'rb')
 
@@ -8,8 +32,25 @@ def read_tweets(fname):
         tweets.append(line[1:len(line) - 2])
 
     print str(tweets[0:10])
+    print len(tweets)
+
     return tweets
 
 
+# TODO:
+# At some point in here we need to extract links (cause that'll be annoying)
+# Links are either retweets or photos right?
 
-read_tweets('globestweets.txt')
+
+def tokenize(a_tweet):
+    """ Method to pass a sentence to NLTK and tokenize it.
+        returns: a list of words in the input sentence
+    """
+    tknzr = TweetTokenizer()
+
+    tokens = tknzr.tokenize(a_tweet)
+    return tokens
+
+
+tweets = read_tweets('globestweets.txt')
+print tokenize(tweets[0])
