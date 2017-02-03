ea7a136e20e1bb888b5fd40cd1bf7e97f1e4f215
Daniel Letscher <dpletscher@outlook.com>
2017-02-03 16:46:28 -0600

    Initial get_presenters Does a decent job. Issues: gets lots of false positives for some awards,, unable to find any presenters from some awards, and Twitter handles need to be converted to names so that it doesn't double up (e.g. Carrie Underwood also referred to by @CarrieUnderwood, @Carrie_UK, etc.)

---
+++ get_presenters.py
@@ -0,0 +1,35 @@
+import globes_tweets as gt
+import get_award_relationships as ga
+import name_proc as np
+import json
+import time
+
+def get_presenters(award_to_tweets):
+    categories_to_presenters = {}
+    np.initialize_names()
+    for category in award_to_tweets:
+        categories_to_presenters[category] = get_presenters_of_category(award_to_tweets[category])
+    return categories_to_presenters
+
+def get_presenters_of_category(tweets):
+    presenters = []
+    presenter_totals = {}
+    presenter_keywords = ['presents', 'present', 'presenting', 'introduce', 'introduces', 'introducing', 'presenters', 'presenter']
+    for tweet in tweets:
+        start = time.time()
+        if any(keyword in tweet['text'] for keyword in presenter_keywords):
+            for name in np.get_names(tweet['text']):
+                if name in presenter_totals.keys():
+                    presenter_totals[name] += 1
+                else:
+                    presenter_totals[name] = 1
+    for p in presenter_totals:
+        if presenter_totals[p] > 5: # TODO placeholder value
+            presenters.append(p)
+    return presenters
+
+def main():
+    tweets = gt.read_tweets_with_metadata('goldenglobes.tab')
+    with open('categories.json') as categories_file:
+        categories = json.load(categories_file)
+    return get_presenters(ga.get_award_relationships(tweets, categories))
