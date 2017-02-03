9639a396ad7ac73200790a40a10da7da7cfc9056
Connor Bain <connorbain@u.northwestern.edu>
2017-02-03 15:49:32 -0600

    New name_proc file
    Use :
    
    from name_proc import *
    
    Then use:
    
    intialize_names()
    get_name(blah)

--- name_proc.py
+++ name_proc.py
@@ -10,6 +10,13 @@ import time
 
 BABY_NAMES = []
 
+def initialize_names():
+    # Load in the baby names
+    with open('yob2015.txt','r') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            BABY_NAMES.append(row[0])
+
 def tokenize(aTweet):
     """ Method to pass a sentence to NLTK and tokenize it.
         returns: a list of words in the input sentence
@@ -20,12 +27,14 @@ def tokenize(aTweet):
     return tokens
 
 def get_names(aTweet):
-    print aTweet
+
     possible_names = set()
     tokens = tokenize(aTweet)
     for i in range(len(tokens) - 1):
         if (tokens[i] in BABY_NAMES) and tokens[i+1][0].isupper() and tokens[i+1].isalnum():
             possible_names.add((" ".join([tokens[i], tokens[i + 1]])))
+        elif tokens[i][0] == "@":
+            possible_names.add(tokens[i])
 
     return possible_names
 
@@ -33,15 +42,9 @@ def get_names(aTweet):
 if __name__ == "__main__":
     #do the thing
     tweets = read_tweets_with_metadata('goldenglobes.tab')
-
-    # Load in the baby names
-    with open('yob2015.txt','r') as f:
-        reader = csv.reader(f)
-        for row in reader:
-            BABY_NAMES.append(row[0])
-
-    BABY_NAMES = BABY_NAMES[:1000]
+    #BABY_NAMES = BABY_NAMES[:1000]
+    print get_names("Vince Vaughn hello hellow hello.")
     # EXAMPLE:
     for aTweet in tweets:
         if aTweet['text'][:2] != 'RT':
-            print get_names(aTweet['text'])
+            pass
