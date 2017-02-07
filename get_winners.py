eb51e0f441c01ad4e10b6236695fb98872dbd15e
samirjoshi <samirjoshi2018@u.northwestern.edu>
2017-02-06 18:56:48 -0600

    runner file

--- get_winners.py
+++ get_winners.py
@@ -3,11 +3,9 @@ import get_award_relationships as ga
 import name_proc as np
 import json
 import operator
-from twitter_connect import *
+# from twitter_connect import *
 
-def getTweetsByCategory():
-    np.initialize_names()
-    tweets = gt.read_tweets_with_metadata('goldenglobes.tab')
+def getTweetsByCategory(tweets):
     with open('categories.json') as categories_file:
         categories = json.load(categories_file)
     return ga.get_award_relationships(tweets, categories)
@@ -27,11 +25,11 @@ def findWinners(tweetsByCat):
                     potential_dict[name] = 1
         # print potential_dict
         award_winner = max(potential_dict.iteritems(), key = operator.itemgetter(1))[0]
-        if award_winner[0] == '@':
-            award_winner_name = lookup_handle(award_winner)
-        else:
-            award_winner_name = award_winner
-        print "AWARD: ", key, ", WINNER:", award_winner_name
+        # if award_winner[0] == '@':
+        #     award_winner_name = lookup_handle(award_winner)
+        # else:
+        #     award_winner_name = award_winner
+        print "AWARD: ", key, ", WINNER:", award_winner
         awards[key] = award_winner
     return awards
 
---
+++ gg_runner.py
@@ -0,0 +1,21 @@
+import globes_tweets as gt
+import get_award_relationships as ga
+import name_proc as np
+import json
+import operator
+import get_presenters as gp
+import get_hosts as gh
+import get_winners as gw
+
+
+
+if __name__ == "__main__":
+  np.initialize_names()
+  tweets = gt.read_tweets_with_metadata('goldenglobes.tab')
+  #get hosts
+  host = gh.findHosts(tweets)
+  print "HOST:", host
+  #get winners
+  tweets_by_cat = gw.getTweetsByCategory(tweets)
+  award_winners = gw.findWinners(tweets_by_cat)
+  print "WINNERS:", str(award_winners)
