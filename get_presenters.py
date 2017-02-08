b962ad41835cf746195c365e63b515047ec2565f
Daniel Letscher <dpletscher@outlook.com>
2017-02-08 14:54:36 -0600

    get_presenters progress only correctly identifies a few presenters... corpus seems to be very incomplete when it comes to tweets about the presenters

--- get_presenters.py
+++ get_presenters.py
@@ -2,6 +2,7 @@ import globes_tweets as gt
 import get_award_relationships as ga
 import name_proc as np
 import json
+import twitter_connect as tc
 
 def get_presenters(award_to_tweets):
     categories_to_presenters = {}
@@ -17,17 +18,24 @@ def get_presenters_of_category(tweets):
     for tweet in tweets:
         if any(keyword in tweet for keyword in presenter_keywords):
             for name in np.get_names(tweet):
-                if name in presenter_totals.keys():
-                    presenter_totals[name] += 1
+                translated_name = name
+                if name[0] == '@':
+                    translated_name = tc.lookup_handle(name)
+                elif name[0] == '#':
+                    translated_name = name[1:]
+                if translated_name in presenter_totals.keys():
+                    presenter_totals[translated_name] += 1
                 else:
-                    presenter_totals[name] = 1
+                    presenter_totals[translated_name] = 1
     for p in presenter_totals:
-        if presenter_totals[p] > 5: # TODO placeholder value
+        if presenter_totals[p] > 1: # TODO placeholder value
             presenters.append(p)
     return presenters
 
-def main():
+if __name__ == "__main__":
     tweets = gt.read_tweets_with_metadata('goldenglobes.tab')
     with open('categories.json') as categories_file:
         categories = json.load(categories_file)
-    return get_presenters(ga.get_award_relationships(tweets, categories))
+    categories_to_presenters = get_presenters(ga.get_award_relationships(tweets, categories))
+    for category in categories_to_presenters:
+        print 'AWARD:', category, ', PRESENTERS:', ', '.join(categories_to_presenters[category])
