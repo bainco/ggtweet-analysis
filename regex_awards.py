b774ff366e3c647160cc9cc29683bc89a5b271b6
Daniel Letscher <dpletscher@outlook.com>
2017-02-12 21:20:42 -0600

    add accept list to regex_awards

--- regex_awards.py
+++ regex_awards.py
@@ -5,11 +5,14 @@ from globes_tweets import *
 tweets = read_tweets_with_metadata('globestweets.txt')
 awardsRegEx = r"Best(?=\s[A-Z])(\s([A-Z]\w+|in|an|a|for|or|-))+"
 
+# stop = ['and', 'a', 'in', '-', 'for']
+accept = ['Drama', 'Musical', 'Picture', 'Director', 'Screenplay', 'Score', 'Song', 'Film', 'Series', 'Television']
+
 categories = set()
 
 for tweet in tweets:
     test = re.search(awardsRegEx, tweet['text'])
-    if test:
+    if test and test.group(0).rsplit(None, 1)[-1] in accept:
         categories.add(test.group(0))
 
 for category in categories:
