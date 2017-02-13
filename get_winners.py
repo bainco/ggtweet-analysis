873dfa19286ac7aa50df2e3c4107e3c82f495f46
SamirJoshi <samirjoshi2018@u.northwestern.edu>
2017-02-13 06:09:12 -0600

    nominees

--- get_winners.py
+++ get_winners.py
@@ -3,7 +3,7 @@ import get_award_relationships as ga
 import name_proc as np
 import json
 import operator
-from twitter_connect import *
+# from twitter_connect import *
 
 def getTweetsByCategory(tweets):
     with open('categories.json') as categories_file:
@@ -23,19 +23,44 @@ def findWinners(tweetsByCat):
                     potential_dict[name] += 1
                 else:
                     potential_dict[name] = 1
-        # print potential_dict        
+        # print "POT:", potential_dict
         award_winner = max(potential_dict.iteritems(), key = operator.itemgetter(1))[0]
-        if award_winner[0] == '@':
-            award_winner_name = lookup_handle(award_winner)
-        elif award_winner[0] == '#':
-            award_winner_name = award_winner[1:]
-        else:
-            award_winner_name = award_winner
-        print "AWARD: ", key, ", WINNER:", award_winner_name
-        awards[key] = award_winner_name
+        # if award_winner[0] == '@':
+        #     award_winner_name = lookup_handle(award_winner)
+        # elif award_winner[0] == '#':
+        #     award_winner_name = award_winner[1:]
+        # else:
+        #     award_winner_name = award_winner
+        print "AWARD: ", key
+        print "        WINNER:", award_winner
+        awards[key] = award_winner
+        winner_lower = award_winner.lower().replace(" ", "")
+        # print "award_lower:", winner_lower
+        del potential_dict[award_winner]
+        nominees = []
+        # print str(potential_dict)
+        while len(nominees) != 4:
+            len_dict = len(potential_dict)
+            if len_dict == 0:
+                break
+            nom_key =  max(potential_dict.iteritems(), key = operator.itemgetter(1))[0]
+            nom_key = str(nom_key)
+            # print "NOM_KEY:", nom_key
+            if ((nom_key[0] != '#')
+                and (nom_key[0] != '@')
+                and ("golden" not in nom_key.lower())
+                and ("globes" not in nom_key.lower())
+                and (winner_lower not in nom_key.lower().replace(" ", ""))):
+                nominees.append(nom_key)
+            del potential_dict[nom_key]
+
+        print "        nominees:", str(nominees)
+
     return awards
 
 
 if __name__ == "__main__":
-    tweetsByCat = getTweetsByCategory()
+    np.initialize_names()
+    tweets = gt.read_tweets_with_metadata('goldenglobes.tab')
+    tweetsByCat = getTweetsByCategory(tweets)
     a = findWinners(tweetsByCat)
--- regex_awards.py
+++ regex_awards.py
@@ -5,8 +5,9 @@ from globes_tweets import *
 tweets = read_tweets_with_metadata('globestweets.txt')
 awardsRegEx = r"Best(?=\s[A-Z])(?:\s([A-Z]\w+|in|an|a))+"
 
+
 for tweet in tweets:
     test = re.search(awardsRegEx, tweet['text'])
     if test:
         print test.group(0)
-        print test.group(1)
+        # print test.group(1)
