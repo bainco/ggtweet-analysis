4f6500aa2d0263ebd39ecf816b83e29c31c0ec88
Connor Bain <connorbain@u.northwestern.edu>
2017-02-12 21:56:08 -0500

    Add possible regex awards

---
+++ regex_awards.py
@@ -0,0 +1,12 @@
+import re
+from globes_tweets import *
+
+
+tweets = read_tweets_with_metadata('globestweets.txt')
+awardsRegEx = r"Best(?=\s[A-Z])(?:\s([A-Z]\w+|in|an|a))+"
+
+for tweet in tweets:
+    test = re.search(awardsRegEx, tweet['text'])
+    if test:
+        print test.group(0)
+        print test.group(1)
