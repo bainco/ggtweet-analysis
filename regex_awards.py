52790ca9c2457d24abecc053bdace210301501dc
Connor Bain <connorbain@u.northwestern.edu>
2017-02-12 22:10:15 -0500

    update regex

--- regex_awards.py
+++ regex_awards.py
@@ -3,10 +3,14 @@ from globes_tweets import *
 
 
 tweets = read_tweets_with_metadata('globestweets.txt')
-awardsRegEx = r"Best(?=\s[A-Z])(?:\s([A-Z]\w+|in|an|a))+"
+awardsRegEx = r"Best(?=\s[A-Z])(\s([A-Z]\w+|in|an|a|for|or|-))+"
+
+categories = set()
 
 for tweet in tweets:
     test = re.search(awardsRegEx, tweet['text'])
     if test:
-        print test.group(0)
-        print test.group(1)
+        categories.add(test.group(0))
+
+for category in categories:
+    print category
